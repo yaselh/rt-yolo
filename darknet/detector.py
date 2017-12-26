0bc7ad57425f4a1658e96a6e096db6d67c6eb046
yassinel <yassineelhimer@gmail.com>
2017-12-26 08:21:27 +0100

    initial update detector.py to read the images dir from command line

---
+++ darknet/detector.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import sys, os
+import math
+sys.path.append(os.path.join(os.getcwd(),'darknet/python/'))
+import darknet as dn
+import numpy as np
+import cv2
+import glob
+
+if __name__ == "__main__":
+	if(len(sys.argv) < 2):
+		print "Please provide the images directory"
+		sys.exit()
+
+	
+	#Define the model, model's meta data and weights
+	model = "cfg/yolo.cfg"
+	metas = "cfg/coco.data"
+	weights = "weights/yolo.weights"
+	#model = "cfg/yolo9000.cfg"
+	#metas = "cfg/combine9k.data"
+	#weights = "weights/yolo9000.weights"
+
+	#load the net
+	net = dn.load_net(model, weights, 0)
+	meta = dn.load_meta(metas)
+
+	#get the images
+	path = argv[1]
+	imgs = glob.glob(path+'/*.jpg')
+
+	#create the output directory
+	if imgs:
+		output_dir = os.path.dirname(imgs[0]) + "/predictions"
+		if not os.path.exists(output_dir):
+			os.makedirs(output_dir)
+
+	#perform the detection
+	for img in imgs: 
+		#r = dn.detect(net, meta, img)
+		output = output_dir + '/' + os.path.basename(img)[:-4]
+		dn.test_detector(net, metas, img, .24, .5, output, False)
+
+
