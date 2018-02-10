1efa1ef4ba67090a5095bf503ea4b2195800fc2d
yassinel <yassineelhimer@gmail.com>
2018-02-10 22:36:51 +0100

    prepare detector to be used from other components

--- darknet/detector.py
+++ darknet/detector.py
@@ -8,8 +8,42 @@ import numpy as np
 import cv2
 import glob
 import argparse
+from multiprocessing.pool import ThreadPool
+
+class Detector:
+	def __init__(self, model="cfg/yolo.cfg",
+					   metas="cfg/coco.data",
+					   weights="weights/yolo.weights"):
+		#Define the model, model's meta data and weights
+		self.model = model
+		self.metas = metas
+		self.weights = weights
+
+		#load the net
+		self.net = dn.load_net(model, weights, 0)
+		self.meta = dn.load_meta(metas)
+
+	def detect(self, img):
+		return dn.detect(self.net, self.meta, img)
+
+	def test(self, imgs_path):
+		#get the images
+		imgs = glob.glob(args.imgs_path+'/*.jpg')
+
+		#create the output directory
+		if imgs:
+			output_dir = os.path.dirname(imgs[0]) + "/predictions"
+			if not os.path.exists(output_dir):
+				os.makedirs(output_dir)
+
+		#perform the detection
+		for img in imgs:
+			#r = dn.detect(net, meta, img)
+			output = output_dir + '/' + os.path.basename(img)[:-4]
+			dn.test_detector(self.net, self.metas, img, .40, .5, output, False)
 
 if __name__ == "__main__":
+
 	parser = argparse.ArgumentParser(description='Detect ojbects and draw the predicted Bounding Boxes')
 
 	parser.add_argument('--imgs_path', type=str,   help='path to the images', required=True)
@@ -20,28 +54,9 @@ if __name__ == "__main__":
 	args = parser.parse_args()
 
 	#Define the model, model's meta data and weights
-	model = args.cfg_path
-	metas = args.data_path
-	weights = args.weights_path
-	#model = "cfg/yolo9000.cfg"
-	#metas = "cfg/combine9k.data"
-	#weights = "weights/yolo9000.weights"
+	model = args.cfg_path #model = "cfg/yolo9000.cfg"
+	metas = args.data_path #metas = "cfg/combine9k.data"
+	weights = args.weights_path #weights = "weights/yolo9000.weights"
 
-	#load the net
-	net = dn.load_net(model, weights, 0)
-	meta = dn.load_meta(metas)
-
-	#get the images
-	imgs = glob.glob(args.imgs_path+'/*.jpg')
-
-	#create the output directory
-	if imgs:
-		output_dir = os.path.dirname(imgs[0]) + "/predictions"
-		if not os.path.exists(output_dir):
-			os.makedirs(output_dir)
-
-	#perform the detection
-	for img in imgs:
-		#r = dn.detect(net, meta, img)
-		output = output_dir + '/' + os.path.basename(img)[:-4]
-		dn.test_detector(net, metas, img, .24, .5, output, False)
+	detector = Detector(model, metas, weights)
+	detector.test(args.imgs_path)
