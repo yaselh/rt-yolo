7217e46cb9e715ced459ea5056ef51dae5852f4b
yassinel <yassineelhimer@gmail.com>
2018-02-11 04:43:04 +0100

    fix installation script

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 echo "Installing the requirements ..."
 pip install -r requirements.txt
-cd rt-yolo/darknet
+cd darknet
 echo "Compiling darknet ..."
 make
 mkdir weights
 echo "Downloading the weights ..."
 wget -P weights/ https://pjreddie.com/media/files/tiny-yolo.weights
+cd ../src/
