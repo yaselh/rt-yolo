2d93532c70e812cbb5422186bb34f1f078f12f92
yassinel <yassineelhimer@gmail.com>
2018-02-16 04:06:44 +0100

    add saving predictions to detector

--- src/detector.py
+++ src/detector.py
@@ -8,6 +8,7 @@ import darknet as dn
 import numpy as np
 import glob
 import argparse
+import cv2
 
 class Detector:
 	def __init__(self,model=darknet_path+"cfg/tiny-yolo.cfg",
@@ -21,6 +22,18 @@ class Detector:
 		#load the net
 		self.net = dn.load_net(model, weights, 0)
 		self.meta = dn.load_meta(metas)
+
+	@staticmethod
+	def draw_bboxes(detected_objects, frame):
+		bboxes = []
+		for r in detected_objects:
+		    cx,cy,w,h = r[2]
+		    x, y = int(cx - w/2), int(cy - h/2)
+		    h, w = int(h), int(w)
+
+		    font = cv2.FONT_HERSHEY_SIMPLEX
+		    cv2.putText(frame, r[0], (x+15,y+15), font, 0.5, (0,255,0), 1, cv2.LINE_AA)
+		    cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),3)
 
 	def detect(self, img):
 		return dn.detect(self.net, self.meta, img)
@@ -40,9 +53,16 @@ class Detector:
 
 		#perform the detection
 		for img in imgs:
+			#detect
 			r = self.detect(img)
 			print r
-			output = output_dir + '/' + os.path.basename(img)[:-4]
+			#save
+			output = output_dir + '/' + os.path.basename(img)
+			img = cv2.imread(img)
+			Detector.draw_bboxes(r, img)
+			print output
+			cv2.imwrite(output, img)
+
 if __name__ == "__main__":
 
 	parser = argparse.ArgumentParser(description='Detect ojbects and draw the predicted Bounding Boxes')
